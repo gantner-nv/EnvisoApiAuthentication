204cef60a52e16e033ebd91deb62f3bb600f5337
TimGeDev <timgebauer.ldr@gmail.com>
2022-07-25 15:35:46 +0200

    ♻️ Improving token generation with renewal and adding pay method

--- php/src/ApiClient.php
+++ php/src/ApiClient.php
@@ -2,31 +2,75 @@
 
 namespace Enviso;
 
-use Illuminate\Http\Client\Response;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
 use phpseclib3\Crypt\RSA;
 use phpseclib3\Crypt\RSA\PublicKey;
 
 class ApiClient
 {
     private string $baseUrl;
+    private string $tenantSecret;
     private string $apiKey;
     private string $publicKey;
 
     public function __construct(
         string $baseUrl,
+        string $tenantSecret,
         string $apiKey,
         string $publicKey,
     )
     {
         $this->baseUrl = $baseUrl;
+        $this->tenantSecret = $tenantSecret;
         $this->apiKey = $apiKey;
         $this->publicKey = $publicKey;
     }
 
+    // If no token exists, create one. If one exists but the 60min long lived cache entry doesn't indicate its active anymore, renew it.
     private function getAuthToken(): string
     {
+        if (!Cache::has('envisopay.authToken')) {
+            $this->createNewToken();
+        } elseif (!Cache::has('envisopay.authTokenStillActive')) {
+            $this->renewExistingToken();
+        }
+
+        return Cache::get("envisopay.authToken");
+    }
+
+    private function renewExistingToken(): void
+    {
+        $authToken = Cache::get("envisopay.authToken");
+        $refreshToken = Cache::get("envisopay.refreshToken");
+
+        if (Str::contains($this->baseUrl, "staging")) {
+            $authEndpoint = $this->baseUrl . "resellingapi/v1/renew";
+        } else {
+            $authEndpoint = $this->baseUrl . "authenticationapi/v1/renew";
+        }
+        $renewal = Http::withHeaders([
+            'x-api-key' => $this->apiKey,
+            'Authorization' => 'bearer ' . $authToken])
+            ->post($authEndpoint,
+                [
+                    'refreshToken' => $refreshToken,
+                ]
+            );
+
+        if ($renewal->ok()) {
+            Cache::put("envisopay.authTokenStillActive", true, 60 * 60); // If one hour has passed, its due to renewal
+        } else {
+            $this->createNewToken();
+        }
+
+    }
+
+    private function createNewToken(): void
+    {
+
         $timestamp = Carbon::now(new \DateTimeZone('UTC'))->isoFormat('YYYY-MM-DDTHH:mm:ss.SSS\Z');
 
         $hash = hash('sha256', $this->apiKey . '_' . $timestamp);
@@ -35,17 +79,24 @@ class ApiClient
         $rsa = PublicKey::loadPublicKey($this->publicKey)->withPadding(RSA::ENCRYPTION_PKCS1);
 
         $signature = base64_encode($rsa->encrypt($hash));
+        if (Str::contains($this->baseUrl, "staging")) {
+            $authEndpoint = $this->baseUrl . "resellingapi/v1/apis/login";
+        } else {
+            $authEndpoint = $this->baseUrl . "authenticationapi/v1/apis/login";
+        }
 
-        $response = Http::acceptJson()->post($this->baseUrl . "resellingapi/v1/apis/login", [
+
+        $response = Http::acceptJson()->post($authEndpoint, [
             'apikey' => $this->apiKey,
             'timestamp' => $timestamp,
             'signature' => $signature
         ]);
 
         if ($response->ok()) {
-            return $response->json('authToken', '');
+            $result = $response->json();
+            Cache::put("envisopay.authToken", $result["authToken"], 60 * 60 * 24 * 30); // Cached for 30 days
+            Cache::put("envisopay.refreshToken", $result["refreshToken"], 60 * 60 * 24 * 30); // Cached for 30 days
         }
 
-        return '';
     }
 }
