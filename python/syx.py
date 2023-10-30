0dba0a21eac936a7b697ca73010043869ee05ce6
nipatel-gantner <148450262+nipatel-gantner@users.noreply.github.com>
2023-10-30 16:16:11 +0530

    Update syx.py | Updated staging creds

--- python/syx.py
+++ python/syx.py
@@ -2,43 +2,47 @@ import base64
 import datetime
 import hashlib
 
+from datetime import datetime, timezone
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_v1_5
 from Crypto.Cipher import PKCS1_OAEP
+
 import requests
 
 pub_key = """-----BEGIN PUBLIC KEY-----
-MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCrtiJ5z5yHWJP7DKjhRjnQbkyY
-e1Ijn1wP2UzSBAfxEBekDbMvw2T+BK6TZhxHzKX9IsTF8Vo8PUmNbomW6Qohd321
-hgairRZ+BG0d7UQVdzB63r2QSbiNmvoayNB93LcITCOxrkXB3fyK7Edv66jF9pTs
-l0mXSGZ0K7UzB28yHQIDAQAB
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDRhyZgq+5gKBOoD5JAstVS5UM3
+/nyl3JAubhEngWvYdzr7+0CSs4gevv+r4CLhkyWZuBNf/B7kXtg0SPJ/Lyp+diJI
+4G0igA/ZT9jpdiT6GIugIwggeuIv0/ykEJMQBeSWQ29DgPf/hwO/PtRWGcOx2NxN
+wh0ULIebM5cEzr3v4QIDAQAB
 -----END PUBLIC KEY-----"""
-api_key = 'KZ9vFiwKh0mlZrq+sxSEbg=='
+api_key = 'h6ISMteE8k2+0thnmHJihg=='
+url = 'https://api.staging-enviso.io/authenticationapi/v1/login/'
 
-timestamp = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')[:-4]+'Z'
+timestamp = datetime.now(timezone.utc).strftime('%Y-%m-%dT%H:%M:%S.%fZ')[:-4]+'Z'
 
-print "timestamp:", timestamp
+print("TIMESTAMP: ", timestamp, "\n")
 key = api_key + '_' + timestamp
 
-print "message", key
+print("MESSAGE: ", key, "\n")
 
-sha_hash = hashlib.sha256(key).hexdigest()
+sha_hash = hashlib.sha256(key.encode()).hexdigest()
 
-print "sha256 hash:", sha_hash
-
+print("SHA256 HASH: ", sha_hash, "\n")
 
 rsa_key = RSA.importKey(pub_key)
 cipher = PKCS1_v1_5.new(rsa_key)
-signature = base64.encodestring(cipher.encrypt(sha_hash))
 
-print "signature:", signature
+signature = base64.b64encode(cipher.encrypt(sha_hash.encode()))
+
+print("SIGNATURE: ", signature, "\n")
 
 response = requests.post(
-    'https://api.staging-enviso.io/resellingapi/v1/apis/login',
+    url,
     json={
         'apikey': api_key,
         'timestamp': timestamp,
-        'signature': signature,
-    },
+        'signature': signature.decode(),
+    }, headers = {'x-api-key': api_key, 'Content-Type': 'application/json',}
 )
-print response.json()
+
+print ("RESPONSE: ", response.json(), "\n")
